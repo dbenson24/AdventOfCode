965233db773c3fb63ae6e3fadbdc093f7a625ede
Derek Benson <derekbenson24@gmail.com>
2021-12-21 11:01:11 -0500

    2019 day12 testing

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     let days = 256;
     //year2021::day6::calc_fish_buckets(days);
     let now = Instant::now();
-    year2021::day21::day21();
+    year2019::day12::day12();
     let elapsed = now.elapsed();
     println!("{} ms", elapsed.as_millis());
     //year2021::day6::calc_fish_recurse(days);
---
+++ src/year2019/day12.rs
@@ -0,0 +1,115 @@
+use crate::utils::*;
+use hashbrown::{HashSet, HashMap};
+use itertools::Itertools;
+
+fn gcd(x: i32, y: i32) -> i32 {
+    let mut x = x;
+    let mut y = y;
+    while y != 0 {
+        let t = y;
+        y = x % y;
+        x = t;
+    }
+    x
+}
+
+fn lcm(a: i32, b: i32) -> i32 {
+    a * b / gcd(a, b)
+}
+
+fn lcm3(a: i32, b: i32, c: i32) -> i32 {
+    lcm(a, lcm(b, c))
+}
+
+
+pub fn day12() {
+    let mut test_moons = [
+        IVec3::new(-1, 0, 2),
+        IVec3::new(2, -10, -7),
+        IVec3::new(4, -8, 8),
+        IVec3::new(3, 5, -1),
+    ];
+    let mut moons = [
+        IVec3::new(-8, -18, 6),
+        IVec3::new(-11, -14, 4),
+        IVec3::new(8, -3, -10),
+        IVec3::new(-2, -16, 1),
+    ];
+
+    let mut vels = [IVec3::ZERO; 4];
+    /*
+    let mut x_states = HashMap::new();
+    let mut y_states = HashMap::new();
+    let mut z_states = HashMap::new();
+
+    let mut x_period: Option<(i32, i32)> = None;
+    let mut y_period: Option<(i32, i32)> = None;
+    let mut z_period: Option<(i32, i32)> = None;
+*/
+    let origin = (moons, vels);
+
+    let mut i = 0usize;
+    loop {
+        /*
+        if x_period.is_none() {
+            let x_state = moons.iter().zip(vels.iter()).map(|(pos, vel)| (pos.x, vel.x)).collect_vec();
+            if let Some(start) = x_states.insert(x_state, i) {
+                x_period = Some((start, i - start));
+            }
+        }
+
+        if y_period.is_none() {
+            let y_state = moons.iter().zip(vels.iter()).map(|(pos, vel)| (pos.y, vel.y)).collect_vec();
+            if let Some(start) = y_states.insert(y_state, i) {
+                y_period = Some((start, i - start));
+            }
+        }
+
+        if z_period.is_none() {
+            let z_state = moons.iter().zip(vels.iter()).map(|(pos, vel)| (pos.z, vel.z)).collect_vec();
+            if let Some(start) = z_states.insert(z_state, i) {
+                z_period = Some((start, i - start));
+            }
+        }
+        
+        if x_period.is_some() && y_period.is_some() && z_period.is_some() {
+            break;
+        }
+        */
+
+        for (i, vel) in vels.iter_mut().enumerate() {
+            let curr_pos = moons[i];
+            for (j, &pos) in moons.iter().enumerate() { 
+                if j != i {
+                    let gravity = (pos - curr_pos).signum();
+                    *vel = *vel + gravity;
+                }
+            }
+        }
+        for i in 0..4 {
+            moons[i] = moons[i] + vels[i];
+        }
+
+        i += 1;
+
+        if origin == (moons, vels) {
+            break;
+        }
+        if i % 10000000 == 0 {
+            dbg!(i);
+        }
+    }
+    dbg!(i);
+    let energy = |x: &IVec3| {
+        let x = x.abs();
+        x.x + x.y + x.z
+    };
+    let e: i32 = moons.iter().zip(vels.iter()).map(|(pos, vel)| energy(pos) * energy(vel)).sum();
+    // let x_period = x_period.unwrap();
+    // let y_period = y_period.unwrap();
+    // let z_period = z_period.unwrap();
+    // dbg!(x_period, y_period, z_period);
+
+    //dbg!(lcm3(x_period.1, y_period.1, z_period.1));
+    dbg!(e);
+}
